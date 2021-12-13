f7c22c4253ebac547f3f8353e09f937aacc7eabb
mateuszkopij <mateusz.kopij@etxcapital.com>
2021-12-13 19:45:44 +0100

    hazelcast playground

---
+++ hazelcast-csharp-client-31/HazelcastPlayground3/HazelcastPlayground3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Hazelcast.Core;
+
+namespace HazelcastPlayground3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start the client and connect to the cluster
+            var hz = HazelcastClient.NewHazelcastClient();
+            // Create a Distributed Map in the cluster
+            var map = hz.GetMap("my-distributed-map");
+            //Standard Put and Get
+            map.put("1", "John");
+            map.put("2", "Mary");
+            map.put("3", "Jane");
+            // Shutdown the client
+            hz.Shutdown();
+        }
+    }
+}
---
+++ hazelcast-csharp/HazelcastPlayground/HazelcastPlayground/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Hazelcast;
+using Hazelcast.Core;
+
+namespace HazelcastPlayground
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await SetData();
+            await GetData();;
+        }
+
+        private static async Task GetData()
+        {
+            var options = new HazelcastOptionsBuilder().Build();
+            options.ClusterName = "hello-world";
+
+            await using var client = await HazelcastClientFactory.StartNewClientAsync(options);
+
+
+            var map = await client.GetMapAsync<string, string>("my-distributed-map");
+            foreach (var entry in await map.GetEntriesAsync())
+                Console.WriteLine($"{entry.Key}: {entry.Value}");
+
+            //client.Shutdown();
+        }
+
+        private static async Task SetData()
+        {
+            var options = new HazelcastOptionsBuilder().Build();
+            options.ClusterName = "hello-world";
+
+            var client = await HazelcastClientFactory.StartNewClientAsync(options);
+
+            var map = await client.GetMapAsync<string, string>("my-distributed-map");
+
+            await map.PutAsync("1", "John");
+            await map.PutAsync("2", "Mary");
+            await map.PutAsync("3", "Jane");
+        }
+    }
+}
