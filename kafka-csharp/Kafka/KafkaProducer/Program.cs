fb04436b7a47ed86a6085d9901af39d1c06e05be
mateuszkopij <mateusz.kopij@etxcapital.com>
2022-01-17 22:28:02 +0100

    introducing kafka producer

---
+++ kafka-csharp/Kafka/KafkaProducer/Program.cs
@@ -0,0 +1,39 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Net;
+
+namespace KafkaProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092,localhost:9092",
+                ClientId = Dns.GetHostName(),
+                
+            };
+
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                var t = producer.ProduceAsync("topic", new Message<Null, string> { Value = "hello world" });
+                t.ContinueWith(task => {
+                    if (task.IsFaulted)
+                    {
+                        
+                    }
+                    else
+                    {
+                        
+
+                        Console.WriteLine($"Wrote to offset: {task.Result.Offset}");
+                    }
+                });
+            }
+        }
+    }
+}
